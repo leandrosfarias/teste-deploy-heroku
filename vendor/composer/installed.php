a1ddc20e9f7c4ce761e6d9291ee51dab5be38754
Leandro Farias <leandro.sfarias997@gmail.com>
2021-11-05 22:29:59 -0300

    ext-pdo

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '9af08159c14068b3db86c137e5b0f43c97ff16d5',
         'name' => 'leandrosfarias/application',
         'dev' => true,
     ),
     'versions' => array(
         'leandrosfarias/application' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '9af08159c14068b3db86c137e5b0f43c97ff16d5',
             'dev_requirement' => false,
         ),
     ),
